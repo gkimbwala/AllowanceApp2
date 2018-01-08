b1c2ba781acf1a9ee466ccfe09bbbbcb34b1185c
Gloria Kimbwala <gkimbwala@gmail.com>
2018-01-07 17:28:13 -0800

    added name, allowance Textview

--- app/src/main/java/com/example/gloria_kimbwala/allowanceapp/MainActivity.java
+++ app/src/main/java/com/example/gloria_kimbwala/allowanceapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.gloria_kimbwala.allowanceapp;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -10,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView nameTextview;
     private TextView allowanceTextview;
+    private RecyclerView choresRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         nameTextview = (TextView) findViewById(R.id.name);
         allowanceTextview = (TextView) findViewById(R.id.allowance);
+        choresRecyclerView = (RecyclerView)findViewById(R.id.chores);
 
         // TODO(gkimbwala): this is a hack that allows us to cheat and run network on the main thread
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
@@ -24,10 +28,12 @@ public class MainActivity extends AppCompatActivity {
 
         try {
             AllowanceServiceStuff allowanceServiceStuff = new AllowanceServiceStuff();
-            AllowanceRate allowanceRate = allowanceServiceStuff.getAllowanceRate("jesse");
+            AllowanceRate allowanceRate = allowanceServiceStuff.getAllowanceRate("gloria");
 
             nameTextview.setText(allowanceRate.name);
             allowanceTextview.setText(Long.toString(allowanceRate.allowance));
+            choresRecyclerView.setAdapter(new ChoresAdapter(allowanceRate.chores));
+            choresRecyclerView.setLayoutManager(new LinearLayoutManager(this));
             Log.i("Allowance", allowanceRate.name + " gets " + allowanceRate.allowance + " for " + allowanceRate.chores);
         } catch (Exception e) {
             Log.w("Allowance", "rate lookup failed", e);
