62e697e28d83b02bfd874ba9ee3fbbc3561883a9
Gloria Kimbwala <gkimbwala@gmail.com>
2018-01-09 10:05:30 -0800

    added name, allowance Textview

---
+++ app/src/main/java/com/example/gloria_kimbwala/allowanceapp/ChoresAdapter.java
@@ -0,0 +1,52 @@
+package com.example.gloria_kimbwala.allowanceapp;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by gloria_kimbwala on 1/6/18.
+ */
+
+public class ChoresAdapter extends RecyclerView.Adapter<ChoresAdapter.ViewHolder>{
+    List<String> chores;
+
+    public ChoresAdapter(List<String> chores) {
+        this.chores = chores;
+    }
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.textViewHead.setText(chores.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return chores.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView textViewHead;
+        public TextView textViewDesc;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            textViewHead = (TextView) itemView.findViewById(R.id.textViewHead);
+            textViewDesc = (TextView) itemView.findViewById(R.id.textViewDesc);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/gloria_kimbwala/allowanceapp/ListItem.java
@@ -0,0 +1,24 @@
+package com.example.gloria_kimbwala.allowanceapp;
+
+/**
+ * Created by gloria_kimbwala on 1/6/18.
+ */
+
+public class ListItem {
+
+    private String head;
+    private String desc;
+
+    public ListItem(String head, String desc) {
+        this.head = head;
+        this.desc = desc;
+    }
+
+    public String getHead() {
+        return head;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+}
